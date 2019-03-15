25d202c15d30471a0f5ab1fe347cc21e854c8bec
Marieme-Fall <marieme.fall@live.fr>
2019-03-14 22:32:59 -0400

    Preparatif Examen

---
+++ PreparationExamen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreparationExamen.Startup))]
+namespace PreparationExamen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
